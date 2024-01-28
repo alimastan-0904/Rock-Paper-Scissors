0e9aa42a5379c25d669338e09fc13816723d7948
Ali Mastan <150234376+alimastan-0904@users.noreply.github.com>
2024-01-28 09:01:35 +0000

    refactor:Amend variable scopes/Put all functions inside a larger one

--- script.js
+++ script.js
@@ -1,74 +1,76 @@
-function getPlayerChoice() {
-  return prompt("Please type in Rock, Paper or Scissors");
-}
+function playRockPaperScissors() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-const weaponChoice = ["Rock", "Scissors", "Paper"];
-function getCompChoice() {
-  return weaponChoice[Math.floor(Math.random() * weaponChoice.length)];
-}
-
-let playerScore = 0;
-let computerScore = 0;
-
-function playRound() {
-  const playerChoice = getPlayerChoice();
-  const computerChoice = getCompChoice();
-  compareChoices(playerChoice, computerChoice);
-}
-
-function compareChoices(playerChoice, computerChoice) {
-  if (playerChoice == computerChoice) {
-    tie(playerChoice, computerChoice);
-  } else if (
-    (playerChoice == "Rock" && computerChoice == "Scissors") ||
-    (playerChoice == "Paper" && computerChoice == "Rock") ||
-    (playerChoice == "Scissors" && computerChoice == "Paper")
-  ) {
-    winRound(playerChoice, computerChoice);
-  } else if (
-    (playerChoice == "Rock" && computerChoice == "Paper") ||
-    (playerChoice == "Paper" && computerChoice == "Scissors") ||
-    (playerChoice == "Scissors" && computerChoice == "Rock")
-  ) {
-    loseRound(playerChoice, computerChoice);
+  function getPlayerChoice() {
+    return prompt("Please type in Rock, Paper or Scissors");
   }
-}
 
-function tie(playerChoice, computerChoice) {
-  alert(
-    `Player chose ${playerChoice} Computer chose ${computerChoice}. It's a tie.`
-  );
-}
+  function getCompChoice() {
+    const weaponChoice = ["Rock", "Scissors", "Paper"];
+    return weaponChoice[Math.floor(Math.random() * weaponChoice.length)];
+  }
 
-function winRound(playerChoice, computerChoice) {
-  alert(
-    `Player chose ${playerChoice} Computer chose ${computerChoice} You win this round.`
-  );
-  playerScore++;
-}
+  function playRound() {
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getCompChoice();
+    compareChoices(playerChoice, computerChoice);
+  }
 
-function loseRound(playerChoice, computerChoice) {
-  alert(
-    `Player chose ${playerChoice} Computer chose ${computerChoice} You lose this round.`
-  );
-  computerScore++;
-}
+  function compareChoices(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+      tie(playerChoice, computerChoice);
+    } else if (
+      (playerChoice == "Rock" && computerChoice == "Scissors") ||
+      (playerChoice == "Paper" && computerChoice == "Rock") ||
+      (playerChoice == "Scissors" && computerChoice == "Paper")
+    ) {
+      winRound(playerChoice, computerChoice);
+    } else if (
+      (playerChoice == "Rock" && computerChoice == "Paper") ||
+      (playerChoice == "Paper" && computerChoice == "Scissors") ||
+      (playerChoice == "Scissors" && computerChoice == "Rock")
+    ) {
+      loseRound(playerChoice, computerChoice);
+    }
+  }
 
-for (let i = 1; i <= 3; i++) {
-  playRound();
-}
+  function tie(playerChoice, computerChoice) {
+    alert(
+      `Player chose ${playerChoice} Computer chose ${computerChoice}. It's a tie.`
+    );
+  }
 
-if (playerScore > computerScore) {
-  alert(
-    `You got ${playerScore} points. Computer got ${computerScore}. You're a Winner`
-  );
-} else if (playerScore < computerScore) {
-  alert(
-    `You got ${playerScore} points. Computer got ${computerScore}. You lose.`
-  );
-} else if (playerScore == computerScore) {
-  alert(
-    `You got ${playerScore} points. Computer got ${computerScore}. It's a tie.
+  function winRound(playerChoice, computerChoice) {
+    alert(
+      `Player chose ${playerChoice} Computer chose ${computerChoice} You win this round.`
+    );
+    playerScore++;
+  }
+
+  function loseRound(playerChoice, computerChoice) {
+    alert(
+      `Player chose ${playerChoice} Computer chose ${computerChoice} You lose this round.`
+    );
+    computerScore++;
+  }
+
+  for (let i = 1; i <= 3; i++) {
+    playRound();
+  }
+
+  if (playerScore > computerScore) {
+    alert(
+      `You got ${playerScore} points. Computer got ${computerScore}. You're a Winner`
+    );
+  } else if (playerScore < computerScore) {
+    alert(
+      `You got ${playerScore} points. Computer got ${computerScore}. You lose.`
+    );
+  } else if (playerScore == computerScore) {
+    alert(
+      `You got ${playerScore} points. Computer got ${computerScore}. It's a tie.
       `
-  );
+    );
+  }
 }
